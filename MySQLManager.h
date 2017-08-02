d9b3ca4215ee0d33174259f3484ed085862ed771
hanhanshangying <1289185169@qq.com>
2017-08-02 15:17:25 +0800

    Add files via upload

---
+++ MySQLManager.h
@@ -0,0 +1,68 @@
+#ifndef MYSQLMANAGER_H_    
+#define MYSQLMANAGER_H_     
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include "MySQLConnectorC/include/mysql.h"    
+#include <string>    
+#include <iostream>    
+#include <vector>    
+#include <string.h>    
+
+using namespace std;
+namespace mySql
+{
+
+	class MySQLManager
+	{
+	public:
+		/*
+		* Init MySQL
+		* @param hosts:         Host IP address
+		* @param userName:        Login UserName
+		* @param password:        Login Password
+		* @param dbName:        Database Name
+		* @param port:                Host listen port number
+		*/
+		MySQLManager(std::string hosts, std::string userName, std::string password, std::string dbName, unsigned int port);
+		~MySQLManager();
+		void initConnection();
+		/*
+		* Making query from database
+		* @param mysql:        MySQL Object
+		* @param sql:                Running SQL command
+		*/
+		bool runSQLQuery(std::string sql);
+		/*
+		* 执行插入语句
+		* @param sql: 执行的SQL语句
+		* @return: 受影响的行数
+		*/
+		unsigned int insert(std::string sql);
+		unsigned int insert1(std::string sql);
+		/**
+		* Destroy MySQL object
+		* @param mysql                MySQL object
+		*/
+		void destroyConnection();
+		bool getConnectionStatus();
+		vector< vector<string> > getResult();
+		MYSQL getMysql(){ return mySQLClient; }
+	protected:
+		void setUserName(std::string userName);
+		void setHosts(std::string hosts);
+		void setPassword(std::string password);
+		void setDBName(std::string dbName);
+		void setPort(unsigned int port);
+		
+	private:
+		bool IsConnected;
+		vector< vector<string> > resultList;
+		MYSQL mySQLClient;
+		unsigned int DEFAULTPORT;
+		char * HOSTS;
+		char * USERNAME;
+		char * PASSWORD;
+		char * DBNAME;
+	};
+}
+#endif /* MYSQLMANAGER_H_ */
